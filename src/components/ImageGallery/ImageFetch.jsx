9a7fe19e43dcd5d5eaecc2e0baa06fa4d4bda71f
xrsQugi <02.04.5.art@gmail.com>
2023-07-10 12:58:09 +0300

    fix

--- src/components/ImageGallery/ImageFetch.jsx
+++ src/components/ImageGallery/ImageFetch.jsx
@@ -1,11 +1,15 @@
 const BASE_URL = 'https://pixabay.com/api';
-const API_KEY = '30848266-1a3122c07315d6e3a00656f38';
-const IMG_TYPE = 'photo';
-const IMG_ORIENT = 'horizontal';
+const API_KEY = '34707727-e20630cf7e49276d83ab15980';
+
+//!Not my API
+// 34707727-e20630cf7e49276d83ab15980
+
+//!My API
+// 30848266-1a3122c07315d6e3a00656f38
 const PER_PAGE = 12;
 
 export default async function GetListImg(query, page) {
-  const url = `${BASE_URL}?image_type=${IMG_TYPE}&orientation=${IMG_ORIENT}&q=${query}&page=${page}&per_page=${PER_PAGE}&key=${API_KEY}`;
+  const url = `${BASE_URL}/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${PER_PAGE}`;
 
   const response = await fetch(url);
   if (response.ok) {
